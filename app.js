5fb5447204535a903c3c82beae14fac49176a92e
Tyler <guerrero.tyler3@gmail.com>
2021-05-13 20:03:48 -0400

    Added Express and MongoDB

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('./config/Config');
+
+const app = express();
+
+const options = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}
+
+mongoose.connect(config.database, options).catch((err) => {
+    console.log(err);
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected');
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/smoothies', (req, res) => {
+    res.render('smoothies');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`)
+})
---
+++ config/Config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    database: "mongodb+srv://tguerr12:unc8sAzI2G3Uc7qY@smoothiecluster.ywd2v.mongodb.net/Smoothie-Server-JWT?retryWrites=true&w=majority"
+}
