17b4da001e6c72e1e0af7ea2d1e4eb196635af96
Tyler <guerrero.tyler3@gmail.com>
2021-05-15 19:24:03 -0400

    Added JWT

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/Config');
 const authRoutes = require('./routes/authRouter')
@@ -33,6 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -42,9 +44,33 @@ app.get('/smoothies', (req, res) => {
     res.render('smoothies');
 })
 
-app.use('/', authRoutes)
+app.use('/', authRoutes);
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`)
 })
+
+// cookies
+// app.get('/set-cookies', (req, res) => {
+//     // res.setHeader('Set-Cookie', 'newUser=true')
+//     res.cookie('newUser', false)
+//     res.cookie('isEmployee', true, {
+//         maxAge: 1000 * 60 * 60 * 24,
+//         // https connection
+//         secure: true,
+//         // wont be able now to access the cookies on the front 
+//         // end only through http protocol
+//         httpOnly: true
+//     })
+//     res.send('you got the cookie')
+// })
+
+// app.get('/read-cookies', (req, res) => {
+//     const cookies = req.cookies;    
+//     console.log(cookies.newUser);
+//     res.status(201).json(cookies)
+// })
--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken')
 
 const handleErrors = (err) => {
     let errors = {email: '', password: ''};
@@ -22,6 +23,14 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+    return jwt.sign({id}, 'TylersSecretshhhhh', {
+        expiresIn: maxAge
+    });
+}
+
 const signup_get = (req, res) => {
     res.render('signup')
 }
@@ -31,7 +40,9 @@ const signup_post = async (req, res) => {
 
     try {   
         const user = await User.create({email, password})
-        res.status(201).json(user)
+        const token = createToken(user._id)
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.status(201).json({user: user._id})
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({errors: errors})
